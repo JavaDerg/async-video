9a78d91c86723a8ea7b4f46c75bf2b1ae8bdde2a
postrex <post-rex@pm.me>
2021-02-22 15:28:31 +0100

    Project rework

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,4 @@
-use crate::manager::Manager;
-use actix::{Actor, Addr};
-use actix_web::error::Error;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
-use actix_web_actors::ws;
+#[tokio::main]
+async fn main() {
 
-mod manager;
-mod msg;
-mod room;
-mod user;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(move || App::new().service(index).data(Manager::default().start()))
-        .bind(std::env::var("INTERFACE").unwrap_or_else(|_| String::from("0.0.0.0:8080")))?
-        .run()
-        .await
-}
-
-#[actix_web::get("/ws/")]
-async fn index(
-    req: HttpRequest,
-    stream: web::Payload,
-    manager: web::Data<Addr<Manager>>,
-) -> Result<HttpResponse, Error> {
-    ws::start(
-        user::UserCon(user::User::new((**manager).clone())),
-        &req,
-        stream,
-    )
 }
