3f1a5c2a6ad482da10e3c57333c75d4dce7493b5
postrex <post-rex@pm.me>
2021-02-22 16:37:22 +0100

    Manually bumped version of warp-embed

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use warp::Filter;
+
+#[derive(rust_embed::RustEmbed)]
+#[folder = "assets"]
+struct Assets;
+
 #[tokio::main]
 async fn main() {
+    let assets = Assets;
+    let assets = warp_embed::embed(&assets)
+        .or(warp_embed::embed_one(&assets, "index.html"));
 
+    warp::serve(assets)
+        .run(([0, 0, 0, 0], 8080))
+        .await;
 }
